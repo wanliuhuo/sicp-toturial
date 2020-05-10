2fd2a7207339d6aef59c2b1277a323e9e8b60638
wanliuhuo <wanliuhuo@gmail.com>
2020-05-10 17:58:56 +0800

    add neg into consideration

--- quickpow.py
+++ quickpow.py
@@ -1,18 +1,26 @@
+class Solution(object):
+    def posPow(self, x, n):
+        result = 1
+        copy = x
+        while n > 0:
+            if n%2 == 1:
+                result = result * copy
+            copy = copy*copy
+            n = n/2
+        return result
 
-def quickPow(base, power):
-    if power == 0:
-        return 1
-    result = 1
-    basecopy = base
-    while power > 0:
-        if power % 2 == 1:
-            result = result * basecopy
-        basecopy = basecopy*basecopy
-        power = power / 2
-    return result
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+        if n > 0:
+            return self.posPow(x, n)
+        if n < 0:
+            return 1.0/(self.posPow(x, -n)*1.0)
 
 
-
-
-out = quickPow(2, 33)
-print(out)
+        
